bfffd555dca44977c07f6fb1f4790c2b93375ec7
José Lucas <joselucas0303@gmail.com>
2019-03-06 00:57:21 -0300

    otimizacao do codigo

--- placarbasquete.py
+++ placarbasquete.py
@@ -31,10 +31,10 @@ class Placar(object):
 
         #Buttons
 
-        self.bt_pontuar = Button(self.janela, text = "Pontuar", font = ('Arial', 14, 'bold'), foreground = 'black', command = partial(self.identificar_time, True))
-        self.bt_voltarponto = Button(self.janela, text = "Voltar pontuação", font = ('Arial', 14, 'bold'), foreground = 'black', command = partial(self.voltarPonto, True))
-        self.bt_finalizar = Button(self.janela, text = "Finalizar", font = ('Arial', 14, 'bold'), foreground = 'black', command = partial(self.finalizar, True))
-        self.bt_alterarLocal = Button(self.janela, text = "Alterar local", font = ("Arial", 10, 'bold'), foreground = 'black', command = partial(self.alterarLocal, True))
+        self.bt_pontuar = Button(self.janela, text = "Pontuar", font = ('Arial', 14, 'bold'), foreground = 'black', command = self.identificar_time)
+        self.bt_voltarponto = Button(self.janela, text = "Voltar pontuação", font = ('Arial', 14, 'bold'), foreground = 'black', command = self.voltarPonto)
+        self.bt_finalizar = Button(self.janela, text = "Finalizar", font = ('Arial', 14, 'bold'), foreground = 'black', command = self.finalizar)
+        self.bt_alterarLocal = Button(self.janela, text = "Alterar local", font = ("Arial", 10, 'bold'), foreground = 'black', command = self.alterarLocal)
 
         #Empacotamento
 
@@ -47,145 +47,133 @@ class Placar(object):
         self.lb_local.place(x = 130, y = 10)
 
 
-    def identificar_time(self, click):
+    def identificar_time(self):
 
-        if click is True:
+        #Destruindo os botões
 
-            #Destruindo os botões
-
-            self.bt_pontuar.place_forget()
-            self.bt_voltarponto.place_forget()
-            self.bt_finalizar.place_forget()
-            self.bt_alterarLocal.place_forget()
+        self.bt_pontuar.place_forget()
+        self.bt_voltarponto.place_forget()
+        self.bt_finalizar.place_forget()
+        self.bt_alterarLocal.place_forget()
             
-            #Criando os CheckButtons e o Label
+        #Criando os CheckButtons e o Label
 
-            self.cbt1 = Checkbutton(self.janela, bg = "black", command = partial(self.identificar_ponto, "casa", True))
-            self.cbt2 = Checkbutton(self.janela, bg = "black", command = partial(self.identificar_ponto, "visitante", True))
-            self.lb = Label(self.janela, text = "Selecione um time", bg = "black", font = ("Arial", 14, "bold"), foreground = "white")
+        self.cbt1 = Checkbutton(self.janela, bg = "black", command = partial(self.identificar_ponto, "casa"))
+        self.cbt2 = Checkbutton(self.janela, bg = "black", command = partial(self.identificar_ponto, "visitante"))
+        self.lb = Label(self.janela, text = "Selecione um time", bg = "black", font = ("Arial", 14, "bold"), foreground = "white")
 
-            #Empacotando
+        #Empacotando
 
-            self.cbt1.place(x = 81, y = 200)
-            self.cbt2.place(x = 371, y = 200)
-            self.lb.place(x = 160, y = 220)
+        self.cbt1.place(x = 81, y = 200)
+        self.cbt2.place(x = 371, y = 200)
+        self.lb.place(x = 160, y = 220)
 
-    def identificar_ponto(self, time, click):
+    def identificar_ponto(self, time):
 
-        if click is True:
+        #Alterando o Label
+        self.time = time
+        self.lb['text'] = "Quantos pontos?"
+        self.lb.place(x = 160, y = 220)
 
-            #Alterando o Label
-            self.time = time
-            self.lb['text'] = "Quantos pontos?"
-            self.lb.place(x = 160, y = 220)
+        #Criando os CheckButtons
 
-            #Criando os CheckButtons
+        self.cbt3 = Checkbutton(self.janela, text = "1 ponto", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 1))
+        self.cbt4 = Checkbutton(self.janela, text = "2 pontos", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 2))
+        self.cbt5 = Checkbutton(self.janela, text = "3 pontos", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 3))
 
-            self.cbt3 = Checkbutton(self.janela, text = "1 ponto", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 1, True))
-            self.cbt4 = Checkbutton(self.janela, text = "2 pontos", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 2, True))
-            self.cbt5 = Checkbutton(self.janela, text = "3 pontos", font = ("Arial", 14, 'bold'), bg = 'black', foreground = 'white', command = partial(self.addPonto, 3, True))
+        #Empacotando
 
-            #Empacotando
+        self.cbt3.place(x =  80, y = 340)
+        self.cbt4.place(x =  200, y = 340)
+        self.cbt5.place(x =  320, y = 340)
 
-            self.cbt3.place(x =  80, y = 340)
-            self.cbt4.place(x =  200, y = 340)
-            self.cbt5.place(x =  320, y = 340)
+    def addPonto(self, ponto):
 
-    def addPonto(self, ponto, click):
 
-        if click is True:
+        self.info[self.time] += ponto
+        self.info['listaPonto'].append({self.time:ponto})
 
-            self.info[self.time] += ponto
-            self.info['listaPonto'].append({self.time:ponto})
+        if self.time == 'casa':
+            self.lbponto_casa['text'] = self.info[self.time]
+        else:
+            self.lbponto_visitante['text'] = self.info[self.time]
+                
+        self.lb.destroy()
+        self.cbt1.destroy()
+        self.cbt2.destroy()
+        self.cbt3.destroy()
+        self.cbt4.destroy()
+        self.cbt5.destroy()
 
+        self.bt_pontuar.place(x = 150, y = 300, width = 200)
+        self.bt_voltarponto.place(x = 150, y = 350, width = 200)
+        self.bt_finalizar.place(x = 150, y = 400, width = 200)
+        self.bt_alterarLocal.place(x = 170, y = 60, width = 150, height = 25)
+            
+    def voltarPonto(self):
+
+        if len(self.info['listaPonto']) == 0:
+            
+            erro = messagebox.showinfo("", "Você não adicionou nenhuma pontuação a base de pontos.")
+
+        else:
+
+            ultimoPonto = len(self.info['listaPonto']) - 1
+
+            for i in self.info['listaPonto'][ultimoPonto].keys():
+
+                self.time = i
+
+            for i in self.info['listaPonto'][ultimoPonto].values():
+
+                self.ponto = i
+
+            self.info[self.time] -= self.ponto
+                
             if self.time == 'casa':
+
                 self.lbponto_casa['text'] = self.info[self.time]
+
             else:
+
                 self.lbponto_visitante['text'] = self.info[self.time]
-                
-            self.lb.destroy()
-            self.cbt1.destroy()
-            self.cbt2.destroy()
-            self.cbt3.destroy()
-            self.cbt4.destroy()
-            self.cbt5.destroy()
-
-            self.bt_pontuar.place(x = 150, y = 300, width = 200)
-            self.bt_voltarponto.place(x = 150, y = 350, width = 200)
-            self.bt_finalizar.place(x = 150, y = 400, width = 200)
-            self.bt_alterarLocal.place(x = 170, y = 60, width = 150, height = 25)
-            
-    def voltarPonto(self, click):
-
-        if click is True:
-
-            if len(self.info['listaPonto']) == 0:
-
-                 erro = messagebox.showinfo("", "Você não adicionou nenhuma pontuação a base de pontos.")
-
-            else:
-
-                ultimoPonto = len(self.info['listaPonto']) - 1
-
-                for i in self.info['listaPonto'][ultimoPonto].keys():
-
-                    self.time = i
-
-                for i in self.info['listaPonto'][ultimoPonto].values():
-
-                    self.ponto = i
-
-                self.info[self.time] -= self.ponto
-                
-                if self.time == 'casa':
-
-                    self.lbponto_casa['text'] = self.info[self.time]
-
-                else:
-
-                    self.lbponto_visitante['text'] = self.info[self.time]
                     
-                self.info['listaPonto'].pop() 
+            self.info['listaPonto'].pop() 
 
-    def finalizar(self, click):
+    def finalizar(self):
 
-        if click is True:
+        if self.info['casa'] > self.info['visitante']:
 
-            if self.info['casa'] > self.info['visitante']:
+            venceu = messagebox.showinfo("", "O time da Casa venceu!")
 
-                venceu = messagebox.showinfo("", "O time da Casa venceu!")
+        elif self.info['casa'] < self.info['visitante']:
 
-            elif self.info['casa'] < self.info['visitante']:
+            venceu = messagebox.showinfo("", "O time dos Visitantes venceram!")
 
-                venceu = messagebox.showinfo("", "O time dos Visitantes venceram!")
+        else:
 
-            else:
+            empate = messagebox.showinfo("", "Empate!")
 
-                empate = messagebox.showinfo("", "Empate!")
+        self.info['casa'] = 0
+        self.info['visitante'] = 0
+        self.lbponto_casa['text'] = self.info['casa']
+        self.lbponto_visitante['text'] = self.info['visitante']
+        self.info['listaPonto'] = []
 
-            self.info['casa'] = 0
-            self.info['visitante'] = 0
-            self.lbponto_casa['text'] = self.info['casa']
-            self.lbponto_visitante['text'] = self.info['visitante']
-            self.info['listaPonto'] = []
-
-    def alterarLocal(self, click):
-
-        if click is True:
+    def alterarLocal(self):
                 
-            self.bt_alterarLocal.place_forget()
-            self.ent = Entry(self.janela)
-            self.bt = Button(self.janela, text = "Confirmar", font = ("Arial", 10, 'bold'), foreground = 'black', command = partial(self.alterar, True)) 
-            self.ent.place(x = 150, y = 60, width = 110)
-            self.bt.place(x = 270, y = 60)
+        self.bt_alterarLocal.place_forget()
+        self.ent = Entry(self.janela)
+        self.bt = Button(self.janela, text = "Confirmar", font = ("Arial", 10, 'bold'), foreground = 'black', command = partial(self.alterar)) 
+        self.ent.place(x = 150, y = 60, width = 110)
+        self.bt.place(x = 270, y = 60)
 
-    def alterar(self, click):
+    def alterar(self):
 
-        if click is True:
-            self.lb_local['text'] = f"Local: {self.ent.get()}"
-            self.ent.destroy()
-            self.bt.destroy()
-            self.bt_alterarLocal.place(x = 170, y = 60, width = 150, height = 25)
+        self.lb_local['text'] = f"Local: {self.ent.get()}"
+        self.ent.destroy()
+        self.bt.destroy()
+        self.bt_alterarLocal.place(x = 170, y = 60, width = 150, height = 25)
         
 janela = Tk()
 Placar(janela, "Indefinido")
